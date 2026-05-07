536581ae05064cdc3de97fb8b03826f645521b58
장선웅 - Jang SeonUng - チャン ソンウン <jsw118536@gmail.com>
2026-05-07 22:22:44 +0900

    Update Constant.java

--- src/main/java/com/example/demo/config/Constant.java
+++ src/main/java/com/example/demo/config/Constant.java
@@ -2,5 +2,5 @@ package com.example.demo.config;
 
 public class Constant {
 
-	public static final String SECRET_KEY = "your_secret_key";
+	public static final String SECRET_KEY = "your_secret_key"; 
 }
